7dae8d06c46cfabbe5e7c56eb6e4774a937c7899
Colin Benner <colin-dev@yzhs.de>
2022-10-02 17:50:35 +0200

    Add function to generate a grid

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use macroquad::prelude::*;
+use macroquad::rand::gen_range;
 
 // Represent the state of the game as a graph.  One node for each cell.  There is an edge between
 // two cells if and only if they are the same colour and have a distance of 1 according to the
@@ -33,11 +34,39 @@ pub enum Colour {
     Fuchsia,
 }
 
+const AllColours: [Colour; 8] = [
+    Colour::Red,
+    Colour::Yellow,
+    Colour::Green,
+    Colour::LightBrown,
+    Colour::Purple,
+    Colour::Cyan,
+    Colour::Blue,
+    Colour::Fuchsia,
+];
+
 struct Grid {
     number_of_rows: u8,
     number_of_columns: u8,
 
     cells: Vec<Colour>,
+}
+
+impl Grid {
+    fn generate(size: usize) -> Self {
+        let mut cells = vec![Colour::Red; size*size];
+        for i in 0..size {
+            for j in 0..size {
+                cells[size * i + j] = AllColours[gen_range(0, AllColours.len())];
+            }
+        }
+
+        Self {
+            number_of_columns: size as u8,
+            number_of_rows: size as u8,
+            cells,
+        }
+    }
 }
 
 struct Position {
