62c53a05b1af9a25f706c5ba49864ab40f126f97
Colin Benner <colin-dev@yzhs.de>
2022-10-03 09:27:41 +0200

    Draw rectangle the size of the grid

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,9 @@ async fn main() {
     loop {
         clear_background(BLACK);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+        let rendering_size = screen_width().min(screen_height());
+
+        draw_rectangle((screen_width() - rendering_size) / 2.0, (screen_height() - rendering_size) / 2.0, rendering_size, rendering_size, GREEN);
 
         next_frame().await
     }
