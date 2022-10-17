1ba6178ee82038be8cc49eba55bc242bdad60ec6
Colin Benner <colin-dev@yzhs.de>
2022-10-17 17:21:22 +0200

    Store number of allowed clicks

--- src/game.rs
+++ src/game.rs
@@ -11,18 +11,24 @@ pub struct Game {
     pub state: GameState,
     pub graph: Graph,
     number_of_clicks: u32,
+    allowed_clicks: u32,
 }
 
 const TOP_LEFT_CELL: Position = Position{column: 0_usize, row: 0_usize};
 
 impl Game {
     pub fn create(size: u32) -> Self {
+        let num_colours = 4;
         let grid = Grid::generate(size as usize);
         let graph = Graph::create(&grid);
+
+        let allowed_clicks = 25 * 2 * size * num_colours / (2 * 14 * 6);
+
         Self {
             state: GameState::Solving,
             graph,
             number_of_clicks: 0,
+            allowed_clicks,
         }
     }
 
